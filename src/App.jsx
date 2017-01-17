2ca65ae542a39792d06cbd25584ffe7b98c561e4
dongyi.an <dongyi.an@zymobi.com>
2017-01-17 09:36:12 +0800

    Revert "update"
    This reverts commit c9315a1f3341b600d52d4b4826cd0b9d142e1a63.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
 import React from "react";
-import Title from "./components/Title.jsx";
-import Header from "./components/Header.jsx";
-import List from "./components/List.jsx";
-import Footer from "./components/Footer.jsx";
-import store from "./store.js";
 import "./style.css";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.addTodo = this.addTodo.bind(this);
-        this.state = {
-            todos: store.get()
-        };
     }
-
-    addTodo(todo) {
-        let todos = this.state.todos;
-        todo.id = Date.now();
-        todos = todos.concat([todo]);
-        this.setState({ todos: todos });
-        store.add(todo);
-    }
-
     render() {
-        return (
-            <div className="container">
-                <Title />
-                <div className="main">
-                    <Header addTodo={this.addTodo} />
-                    <List activeStatus={this.props.params.activeStatus} todos={this.state.todos} />
-                    <Footer />
-                </div>
-            </div>
-        );
+        return <h1>Hello React</h1>;
     }
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, hashHistory } from "react-router";
 import App from "./App.jsx";
 
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Router path="/" component={App}></Router>
-        <Router path="/:activeStatus" component={App}></Router>
-    </Router>,
+    <App />,
     document.getElementById("root")
 );

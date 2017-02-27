c74b7a101db505a82fa4b799592f6c7d438d8d6e
acmilank22 <acmilank22@gmail.com>
2017-02-27 22:40:01 +0800

    update

--- server.bat
+++
@@ -1,2 +0,0 @@
-start node server.js
-start http://127.0.0.1:8080/
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,118 @@
-var path = require("path");
-var webpack = require("webpack");
-var autoprefixer = require("autoprefixer");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer")({ browsers: ["last 100 versions", "> 1%"] });
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpackMerge = require("webpack-merge");
 
-module.exports = {
-    entry: [
-        "whatwg-fetch",
-        "webpack-dev-server/client?http://127.0.0.1:8080",
-        "webpack/hot/only-dev-server",
-        "./src/index.jsx"
-    ],
+const commonEntry = ["./src/index.jsx"];
+const publicPath = "/";
+
+const commonConfig = {
     output: {
-        path: __dirname + "/build/",
-        filename: "bundle.js"
+        path: path.resolve(__dirname, "build")
     },
     module: {
-        loaders: [
-            { test: /\.(js|jsx)$/, loaders: ["react-hot-loader", "babel-loader?presets[]=es2015,presets[]=react"], include: path.join(__dirname, "src") },
-            { test: /\.(css|scss)$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap!postcss-loader!sass-loader?outputStyle=expanded&sourceMap=true&sourceMapContents=true") },
-            { test: /\.(png|jpg)$/, loader: "url-loader?limit=524288" }
-        ]
+        rules: [{
+            test: /\.(js|jsx)$/,
+            use: [{
+                loader: "babel-loader",
+                query: {
+                    presets: [
+                        ["es2015", { "modules": false }],
+                        "react"
+                    ]
+                }
+            }],
+            include: path.join(__dirname, "src")
+        }, {
+            test: /\.(css|scss)$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: [{
+                    loader: "css-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "postcss-loader",
+                    options: {
+                        plugins: function() {
+                            return [autoprefixer];
+                        }
+                    }
+                }, {
+                    loader: "sass-loader",
+                    options: {
+                        outputStyle: "expanded",
+                        sourceMap: true,
+                        sourceMapContents: true
+                    }
+                }]
+            })
+        }, {
+            test: /\.(png|jpg)$/,
+            use: {
+                loader: "url-loader",
+                options: { limit: 524288 }
+            }
+        }]
     },
-    postcss: [autoprefixer({
-        browsers: ["last 100 versions", "> 1%"]
-    })],
     devtool: "source-map",
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({ title: "Hello React", template: __dirname + "/src/index.html" }),
-        new ExtractTextPlugin("styles.css")
+        new HtmlWebpackPlugin({
+            title: "Hello React",
+            template: path.join(__dirname, "/src/index.html")
+        })
     ]
+};
+
+const productionConfig = {
+    entry: commonEntry,
+    output: {
+        filename: "bundle.min.[chunkhash].js"
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.min.[chunkhash].css"),
+        new webpack.optimize.UglifyJsPlugin({
+            comments: false,
+            sourceMap: true
+        }),
+        new webpack.LoaderOptionsPlugin({ minimize: true }),
+        new webpack.DefinePlugin({ "process.env": { "NODE_ENV": JSON.stringify("production") } })
+    ]
+};
+
+const developmentConfig = {
+    output: {
+        publicPath: publicPath
+    },
+    entry: [
+        "react-hot-loader/patch",
+        "webpack-dev-server/client?http://127.0.0.1:8080",
+        "webpack/hot/only-dev-server"
+    ].concat(commonEntry),
+    output: {
+        filename: "bundle.[hash].js"
+    },
+    devServer: {
+        hot: true,
+        contentBase: path.resolve(__dirname, "build"),
+        publicPath: publicPath
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.[hash].css"),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    ]
+};
+
+module.exports = function(env) {
+    let config = null;
+    if (env === "production") {
+        config = webpackMerge(commonConfig, productionConfig);
+    } else {
+        config = webpackMerge(commonConfig, developmentConfig);
+    }
+    return config;
 };

a65d63c4b9f3e7433c996d326669ed6f8111504d
acmilank22 <acmilank22@gmail.com>
2017-05-19 00:58:34 +0800

    add sass

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 
+import "./sass/index.scss";
+
 ReactDOM.render(
     <App />,
     document.getElementById("root")

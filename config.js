7202fa2baad89321a579e337bc7efa355b28561c
Dongyi An <acmilank22@gmail.com>
2018-09-07 14:18:15 +0800

    update

---
+++ config.js
@@ -0,0 +1,11 @@
+const enableProductionSourceMap = false;
+
+const pageList = [
+    {
+        name: 'app', // 输出的js文件名
+        filename: 'index', // 输入的jsx文件名，输出的html文件名
+        template: 'index' // 模板名
+    }
+];
+
+module.exports = { pageList, enableProductionSourceMap };
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,4 @@
-/**
- * User Config
- */
-const enableProductionSourceMap = false;
-const pageList = [
-    {
-        name: 'app', // 输出的js文件名
-        filename: 'index', // 输入的jsx文件名，输出的html文件名
-        template: 'index' // 模板名
-    }
-];
+const { pageList, enableProductionSourceMap } = require('./config');
 
 /**
  * webpack Config

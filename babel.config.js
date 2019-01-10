828edee0fe7f3d663c8a84e298bdb033a300c71f
Dongyi An <acmilank22@gmail.com>
2019-01-10 15:42:56 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,26 @@
-const { prodMode } = require('./config/env');
-
-const plugins = ['react-hot-loader/babel', 'babel-plugin-styled-components'];
-const config = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-        useBuiltIns: 'usage'
-      }
+const config = api => {
+  const prodMode = api.env('production');
+  const commonPlugins = [
+    'react-hot-loader/babel',
+    'babel-plugin-styled-components'
+  ];
+  const plugins = prodMode
+    ? [...commonPlugins, 'transform-react-remove-prop-types']
+    : commonPlugins;
+  const config = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+          useBuiltIns: 'usage'
+        }
+      ],
+      '@babel/preset-react'
     ],
-    '@babel/preset-react'
-  ],
-  plugins: prodMode
-    ? plugins.concat(['transform-react-remove-prop-types'])
-    : plugins
+    plugins
+  };
+  return config;
 };
 
 module.exports = config;

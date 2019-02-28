beb3373db9f29a2e2c25ff5e95332c0caec28ef4
Dongyi An <acmilank22@gmail.com>
2019-02-28 18:46:41 +0800

    update

---
+++ src/components/Tabs/components/Tabs/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { ReactTabs } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+const StyledTabs = styled(ReactTabs)`
+  display: flex;
+  flex-direction: column;
+`;
+
+function Tabs({ children, ...rest }) {
+  return <StyledTabs {...rest}>{children}</StyledTabs>;
+}
+
+export default Tabs;
--- src/routes/Experiment/index.js
+++ src/routes/Experiment/index.js
@@ -6,8 +6,9 @@ import { vw } from '../../styles/helpers';
 import Page from '../../components/Page';
 import FlexCol from '../../components/FlexCol';
 
+import SwipeableViews from 'react-swipeable-views';
 import { useSwipeable } from 'react-swipeable';
-import { Tab, TabList, Tabs, TabPanel } from 'react-tabs';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -16,13 +17,17 @@ const Container = styled.div`
   overflow-x: hidden;
 `;
 
+const StyleTabs = styled(Tabs)`
+  display: flex;
+  flex-direction: column;
+`;
+
 const StyleTabList = styled(TabList)`
   display: flex;
   margin: 0;
   padding: 0;
 
-  /* transition: transform 0.3s ease; */
-  transition: transform 0.35s cubic-bezier(0.15, 0.3, 0.25, 1);
+  transition: transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
   will-change: transform;
 `;
 
@@ -33,6 +38,15 @@ const StyleTab = styled(Tab)`
   flex-shrink: 0;
   width: 25%;
   padding: 20px;
+`;
+
+const StyleTabPanelList = styled.div`
+  display: flex;
+  margin: 0;
+  padding: 0;
+
+  transition: transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  will-change: transform;
 `;
 
 function MyTabList({ children }) {
@@ -73,7 +87,6 @@ function MyTabList({ children }) {
     console.log('swiped', deltaX);
     if (['Left', 'Right'].includes(dir)) {
       const newTranslateX = getNewTranslateX({ dir, deltaX });
-      // setTranslateX(newTranslateX);
       setLastTranslateX(newTranslateX);
     }
   }
@@ -81,8 +94,6 @@ function MyTabList({ children }) {
   const handlers = useSwipeable({
     onSwiping: handleSwiping,
     onSwiped: handleSwiped
-    // onSwipedLeft: handleSwiped,
-    // onSwipedRight: handleSwiped
   });
 
   return (
@@ -103,24 +114,89 @@ function MyTab({ children }) {
   return <StyleTab>{children}</StyleTab>;
 }
 
+function MyTabPane({ children }) {
+  return <StyleTab>{children}</StyleTab>;
+}
+
+function TabPanelList({ children }) {
+  const count = Children.count(children);
+  const minTranslateX = -(count - 1) * screen.width;
+
+  const [translateX, setTranslateX] = useState(0);
+  const [lastTranslateX, setLastTranslateX] = useState(translateX);
+
+  function getNewTranslateX({ dir, deltaX }) {
+    const computedTranslateX = lastTranslateX - deltaX;
+    let newTranslateX = 0;
+    if (dir === 'Left') {
+      if (computedTranslateX > minTranslateX) {
+        newTranslateX = computedTranslateX;
+      } else {
+        newTranslateX = minTranslateX;
+      }
+    } else if (dir === 'Right') {
+      if (computedTranslateX < 0) {
+        newTranslateX = computedTranslateX;
+      } else {
+        newTranslateX = 0;
+      }
+    }
+    return newTranslateX;
+  }
+
+  function handleSwiping(eventData) {
+    const { dir, deltaX } = eventData;
+    console.log('swiping', deltaX);
+    const newTranslateX = getNewTranslateX({ dir, deltaX });
+    setTranslateX(newTranslateX);
+  }
+
+  function handleSwiped(eventData) {
+    const { dir, deltaX } = eventData;
+    console.log('swiped', deltaX);
+    if (['Left', 'Right'].includes(dir)) {
+      const newTranslateX = getNewTranslateX({ dir, deltaX });
+      setLastTranslateX(newTranslateX);
+    }
+  }
+
+  const handlers = useSwipeable({
+    onSwiping: handleSwiping,
+    onSwiped: handleSwiped
+  });
+
+  return (
+    <Container>
+      <StyleTabPanelList
+        {...handlers}
+        style={{ transform: `translate(${translateX}px, 0)` }}
+      >
+        {children}
+      </StyleTabPanelList>
+    </Container>
+  );
+}
+
 MyTab.tabsRole = 'Tab';
 
 function Experiment({ documentTitle }) {
   return (
     <Page documentTitle={documentTitle}>
-      <Tabs>
+      <StyleTabs>
         <MyTabList>
           {list.map((value, index) => (
             <MyTab key={index}>Tab {value}</MyTab>
           ))}
         </MyTabList>
 
-        {list.map((value, index) => (
-          <TabPanel key={index}>
-            <h2>TabPanel {value}</h2>
-          </TabPanel>
-        ))}
-      </Tabs>
+        <TabPanelList>
+          {list.map((value, index) => (
+            <TabPanel key={index} forceRender={true}>
+              <h2>TabPanel {value}</h2>
+            </TabPanel>
+          ))}
+        </TabPanelList>
+      </StyleTabs>
     </Page>
   );
 }

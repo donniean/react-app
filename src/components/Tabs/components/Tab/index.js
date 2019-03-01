b7b61acf902005c4bb67ffa9ca117cd176d94857
Dongyi An <acmilank22@gmail.com>
2019-03-01 16:50:08 +0800

    update

--- src/components/Tabs/components/Tab/index.js
+++ src/components/Tabs/components/Tab/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Measure from 'react-measure';
+import { SizeMe } from 'react-sizeme';
 
 import { Tab as ReactTab } from 'react-tabs';
 
@@ -14,7 +16,19 @@ const StyleTab = styled(ReactTab)`
 `;
 
 function Tab({ children, ...rest }) {
-  return <StyleTab {...rest}>{children}</StyleTab>;
+  const [size, setSize] = useState(null);
+  console.log(size);
+
+  return (
+    <SizeMe>
+      {({ size }) => {
+        console.log(size);
+        return <StyleTab {...rest}>{children}</StyleTab>;
+      }}
+    </SizeMe>
+  );
+
+  // return <StyleTab {...rest}>{children}</StyleTab>;
 }
 
 Tab.defaultProps = {};

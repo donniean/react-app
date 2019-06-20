41253bf876c98032d6b6b2246fcc2c70b4bad113
Dongyi An <acmilank22@gmail.com>
2019-06-20 22:15:49 +0800

    update

--- src/styles/GlobalStyle/index.js
+++ src/styles/GlobalStyle/index.js
@@ -2,23 +2,15 @@ import 'sanitize.css';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-
-  html, body {
-    height: 100%;
-  }
-
   body {
-    display: flex;
-    flex-direction: column;
     padding-bottom: constant(safe-area-inset-bottom);
     padding-bottom: env(safe-area-inset-bottom);
     background-color: #ffffff;
-    user-select: none;
-    -webkit-touch-callout: none;
   }
 
-  h1, h2, h3, h4, h5, h6, p {
+  * {
     margin: 0;
+    padding: 0;
   }
 
   input,
@@ -27,16 +19,6 @@ const GlobalStyle = createGlobalStyle`
     appearance: none;
     outline: 0;
     border: 0;
-  }
-
-  textarea {
-    resize: none;
-  }
-
-  div#root {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
   }
 `;
 

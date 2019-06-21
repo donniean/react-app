ead155f4ed675c9ecc5f75598fdfb54f64301d1e
Dongyi An <acmilank22@gmail.com>
2019-06-21 17:03:27 +0800

    update

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -1,9 +1,12 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 const rewireStyledComponents = require('react-app-rewire-styled-components');
 
+const rewireReactHotLoaderReactDom = require('./react-app-rewire-hot-loader-react-dom');
+
 module.exports = {
   webpack(config, env) {
     config = rewireReactHotLoader(config, env);
+    config = rewireReactHotLoaderReactDom(config, env);
     config = rewireStyledComponents(config, env);
     return config;
   }

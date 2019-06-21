eb1a96725ec618841882fe6154136a91e9046422
Dongyi An <acmilank22@gmail.com>
2019-06-21 15:43:22 +0800

    update

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -1,11 +1,9 @@
-const rewireFaviconsPlugin = require('react-app-rewire-favicons-plugin');
 const rewireStyledComponents = require('react-app-rewire-styled-components');
 
 const faviconsWebpackPluginOptions = require('./faviconsWebpackPluginOptions');
 
 module.exports = {
   webpack(config, env) {
-    config = rewireFaviconsPlugin(config, env, faviconsWebpackPluginOptions);
     config = rewireStyledComponents(config, env);
     return config;
   }

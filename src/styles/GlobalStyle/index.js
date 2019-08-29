eb2eecd366ec8731f1737b270d8cebee3ba93e56
Dongyi An <acmilank22@gmail.com>
2019-08-29 13:47:58 +0800

    update

--- src/styles/GlobalStyle/index.js
+++ src/styles/GlobalStyle/index.js
@@ -5,15 +5,31 @@ import 'sanitize.css/typography.css';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  * {
+  *,
+  *::before,
+  *::after {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
   }
 
+  html {
+    font-size: 100%;
+	  line-height: 1;
+  }
+
   body {
     padding-bottom: constant(safe-area-inset-bottom);
     padding-bottom: env(safe-area-inset-bottom);
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
   }
 
   input,

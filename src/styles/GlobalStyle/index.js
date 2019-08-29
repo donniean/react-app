9f3df2fbeeaa0278a7c91c0ee38293bcfc571cbd
Dongyi An <acmilank22@gmail.com>
2019-08-29 11:27:33 +0800

    update

--- src/styles/GlobalStyle/index.js
+++ src/styles/GlobalStyle/index.js
@@ -5,15 +5,15 @@ import 'sanitize.css/forms.css';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
   body {
     padding-bottom: constant(safe-area-inset-bottom);
     padding-bottom: env(safe-area-inset-bottom);
-    background-color: #fff;
-  }
-
-  * {
-    margin: 0;
-    padding: 0;
   }
 
   input,

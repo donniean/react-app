43e14d11e20b814205f164200437bc7e7c379515
Dongyi An <acmilank22@gmail.com>
2019-09-02 13:56:12 +0800

    update

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -1,13 +1,11 @@
+const { override } = require('customize-cra');
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 const rewireStyledComponents = require('react-app-rewire-styled-components');
 
 const rewireReactHotLoaderReactDom = require('./react-app-rewire-hot-loader-react-dom');
 
-module.exports = {
-  webpack(config, env) {
-    config = rewireReactHotLoader(config, env);
-    config = rewireReactHotLoaderReactDom(config, env);
-    config = rewireStyledComponents(config, env);
-    return config;
-  }
-};
+module.exports = override(rewireReactHotLoaderReactDom(), (config, env) => {
+  config = rewireReactHotLoader(config, env);
+  config = rewireStyledComponents(config, env);
+  return config;
+});

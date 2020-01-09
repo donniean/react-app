3335f8344b7d38b34d661f5d1cdfe40d30f351ae
Dongyi An <acmilank22@gmail.com>
2020-01-09 17:15:35 +0800

    update

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+// TODO: TEST
 import useSWR from 'swr';
 import request from '../../utils/request';
 

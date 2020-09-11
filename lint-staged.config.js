514d5a256faa9d4f869212a577ae7ac46b146a65
Donnie An <acmilank22@gmail.com>
2020-09-11 14:52:36 +0800

    chore: m

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  '*.{js,ts,jsx,tsx,json,html,vue,css,less,scss,md,yaml}': [
-    'prettier --write',
-    'git add',
-  ],
-  '*.{js,jsx,html,vue}': ['eslint --fix', 'git add'],
-  '*.*': ['cspell'],
+  '*.{js,ts,jsx,tsx,json,html,vue,css,less,scss,md,yaml}': 'prettier --write',
+  '*.{js,jsx,html,vue}': 'eslint --fix',
+  '*.*': 'cspell',
 };

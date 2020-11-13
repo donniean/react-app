44ef789d78825520a38f89f14f2f0041990a7c21
Donnie An <acmilank22@gmail.com>
2020-11-13 10:00:58 +0800

    feat: use axios instead of fetch in utils/request

---
+++ src/utils/request/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: '',
+});
+
+axios.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;

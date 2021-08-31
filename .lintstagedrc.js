cd8898d8f1f2091178952bfb9a567806451b6e28
Donnie An <acmilank22@gmail.com>
2021-08-31 14:54:41 +0800

    build: upgrade npm packages

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ module.exports = {
   '*.{js,jsx,css,scss,json,md}': 'prettier --write',
   '*.{js,jsx}': 'eslint --fix',
   '*.{css,scss,js,jsx}': 'stylelint --fix',
-  '**': 'cspell',
+  '**': 'cspell --no-must-find-files',
 };

c299794c00924fe683afd60fc02008dbff5fec7e
Donnie An <acmilank22@gmail.com>
2021-10-29 18:10:28 +0800

    build: upgrade npm packages

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
+  plugins: ['stylelint-order'],
   extends: [
     'stylelint-config-standard',
-    'stylelint-config-rational-order',
+    'stylelint-config-standard-scss',
     'stylelint-config-styled-components',
+    'stylelint-config-rational-order',
     'stylelint-prettier/recommended',
   ],
-  plugins: ['stylelint-order'],
   rules: {
     'color-named': ['never', { ignore: ['inside-function'] }],
-    'font-family-name-quotes': 'always-where-recommended',
     'value-keyword-case': ['lower', { ignoreKeywords: [/^[a-z]+[A-Z][a-z]*/] }],
-    'shorthand-property-no-redundant-values': true,
-    'property-no-vendor-prefix': true,
-    'declaration-block-no-redundant-longhand-properties': true,
-    'selector-attribute-quotes': 'always',
     'selector-list-comma-space-after': 'always-single-line',
     'no-unknown-animations': true,
   },

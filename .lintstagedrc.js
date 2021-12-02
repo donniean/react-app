6e21582594c51990019d21c41af3a0d829c21a64
donniean <acmilank22@gmail.com>
2021-12-02 15:51:12 +0800

    feat: node-config => dotenv

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ module.exports = {
   '*.{js,jsx,ts,tsx,json,hbs,handlebars,css,scss,md}': 'prettier --write',
   '*.{js,jsx,ts,tsx}': 'eslint --fix',
   '*.{css,scss,js,jsx,ts,tsx}': 'stylelint --fix',
-  // '**': 'cspell --no-must-find-files',
+  '**': 'cspell --no-must-find-files',
 };

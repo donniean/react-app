54e4634e2dcacd1b4498ffdfcf3bf8087998a06b
donniean <acmilank22@gmail.com>
2022-01-12 16:38:08 +0800

    feat: modified react imports

--- babel.config.js
+++ babel.config.js
@@ -7,13 +7,12 @@ module.exports = {
       },
     ],
     '@babel/preset-typescript',
-    '@babel/preset-react',
-    /* [
+    [
       '@babel/preset-react',
       {
         runtime: 'automatic',
       },
-    ], */
+    ],
   ],
   plugins: [
     '@babel/plugin-syntax-dynamic-import',

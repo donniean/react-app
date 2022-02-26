d6030056a16bb763cb5ef6a8cd013e9046619b79
donniean <acmilank22@gmail.com>
2022-02-26 20:00:00 +0800

    feat: m

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -8,6 +8,11 @@ declare module '*.module.scss' {
   export default classes;
 }
 
+declare module '*.module.less' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
 declare module '*.svg?svgr' {
   import { FunctionComponent, SVGProps } from 'react';
 

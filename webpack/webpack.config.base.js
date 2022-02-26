85bc18876a40d79165919b35c8f5a651ffa98c0e
donniean <acmilank22@gmail.com>
2022-02-26 16:30:17 +0800

    feat: m

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -27,7 +27,7 @@ const generateSourcemap = config?.builder?.generateSourcemap;
 
 dotenvExpand(dotenvFlow.config());
 
-const getStyleLoaders = () => {
+const getStyleLoaders = ({ type } = {}) => {
   let [sourceMap, modules] = [true, { auto: true }];
 
   if (isEnvDevelopment) {
@@ -58,8 +58,14 @@ const getStyleLoaders = () => {
         sourceMap,
       },
     },
-    {
+    ['sass', 'scss'].includes(type) && {
       loader: 'sass-loader',
+      options: {
+        sourceMap,
+      },
+    },
+    type === 'less' && {
+      loader: 'less-loader',
       options: {
         sourceMap,
       },
@@ -89,8 +95,16 @@ module.exports = {
         loader: 'handlebars-loader',
       },
       {
-        test: /\.(sass|scss|css)$/,
+        test: /\.css$/,
         use: getStyleLoaders(),
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: getStyleLoaders({ type: 'sass' }),
+      },
+      {
+        test: /\.less$/,
+        use: getStyleLoaders({ type: 'less' }),
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,

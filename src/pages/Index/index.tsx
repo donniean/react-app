adfecaad8ed38d555f5309dd45825f701edda6d7
donniean <acmilank22@gmail.com>
2022-11-09 01:29:53 +0800

    feat: upgrade dependencies

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
   return (
     <Wrapper>
       <Logo width={128} height={128} />
-      <Title>Hello React 1</Title>
+      <Title>Hello React</Title>
     </Wrapper>
   );
 }

7dd5a764aae3e703ff836ad96035ec02690837cb
donniean <acmilank22@gmail.com>
2023-03-25 01:17:15 +0800

    feat: delete configs config file

--- configs.config.js
+++ configs.config.js
@@ -1,5 +1,64 @@
 module.exports = {
-  modules: {
+  features: {
+    gitignore: true,
+    gitattributes: true,
+    editorconfig: true,
+    prettier: {
+      extensions: [
+        'js',
+        'jsx',
+        'ts',
+        'tsx',
+        'mjs',
+        'cjs',
+        'json',
+        'html',
+        'vue',
+        'hbs',
+        'handlebars',
+        'css',
+        'less',
+        'scss',
+        'md',
+        'mdx',
+        'yaml',
+        'yml',
+      ],
+    },
+    tsc: {
+      extensions: ['ts', 'tsx'],
+    },
+    eslint: {
+      extensions: ['js', 'jsx', 'ts', 'tsx', 'mjs', 'cjs'],
+      options: {
+        node: true,
+      },
+    },
+    stylelint: {
+      extensions: ['css', 'scss', 'js', 'jsx', 'ts', 'tsx'],
+      options: {
+        scss: true,
+        'css-in-js': true,
+      },
+    },
+    htmlhint: true,
+    markdownlint: true,
+    cspell: {
+      extensions: ['*'],
+    },
+    commitlint: true,
     commitizen: true,
+    'sort-package-json': true,
+    husky: true,
+    'lint-staged': {
+      options: {
+        prettier: true,
+        tsc: true,
+        eslint: true,
+        stylelint: true,
+        markdownlint: true,
+        cspell: true,
+      },
+    },
   },
 };

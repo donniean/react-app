70269c55127a39e7fc0b15818deb2aa071a06e8f
donniean <acmilank22@gmail.com>
2023-04-22 03:55:12 +0800

    ci: modify GitHub Actions (#42)

---
+++ lint-staged.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  'package.json': 'sort-package-json',
+  '*': [
+    'prettier --write --ignore-unknown',
+    'cspell lint --no-progress --relative --no-must-find-files --dot --gitignore',
+  ],
+  '*.{ts,tsx}': 'bash -c tsc --noEmit',
+  '*.{js,mjs,cjs,ts,tsx}': 'eslint --fix',
+  '*.{css,scss,ts,tsx}': 'stylelint --fix',
+  '*.md': 'markdownlint  --dot --fix',
+};

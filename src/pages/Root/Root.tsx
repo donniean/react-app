d3b0861884c19ce7240bf50dbe636ed6d0cbed56
donniean <acmilank22@gmail.com>
2023-05-11 15:26:57 +0800

    chore: update dependencies (#86)

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,11 +1,12 @@
 import Logo from '@/assets/images/logo.svg?svgr';
 
 import { Title, Wrapper } from './Root.styles';
+import styles from './Root.styles.module.scss';
 
 export function Root() {
   return (
     <Wrapper>
-      <Logo width={128} height={128} />
+      <Logo className={styles.logo} width={128} height={128} />
       <Title>Hello React</Title>
     </Wrapper>
   );

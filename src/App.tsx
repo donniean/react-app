2f4a13905ba8428f19ac0d5e0e7e1f420f3cb51e
donniean <acmilank22@gmail.com>
2023-05-17 17:42:58 +0800

    feat: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
+
 import { router } from '@/router';
 
 const queryClient = new QueryClient();

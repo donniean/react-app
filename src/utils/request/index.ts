6dbee88c3732d3b80c543a1dc84fb5788a34cd6d
donniean <acmilank22@gmail.com>
2023-07-07 11:00:56 +0800

    chore: upgrade dependencies (#194)

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -8,14 +8,14 @@ instance.interceptors.request.use(
   config => config,
   error => {
     throw error;
-  }
+  },
 );
 
 instance.interceptors.response.use(
   response => response,
   error => {
     throw error;
-  }
+  },
 );
 
 export { instance as request };

c94b7e8067a17bb38ae246dd5ffff08725e382ab
donniean <acmilank22@gmail.com>
2024-03-03 21:39:36 +0800

    chore: m
    Signed-off-by: donniean <acmilank22@gmail.com>

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,13 +1,13 @@
 export default {
   'package.json': 'sort-package-json',
-  '*': [
-    'prettier --write --ignore-unknown',
-    'cspell lint --no-progress --relative --no-must-find-files --dot --gitignore',
-  ],
-  '*.{ts,tsx}': 'bash -c tsc --noEmit',
-  '*.{js,mjs,cjs,ts,tsx}': 'eslint --fix',
-  '*.css': 'stylelint --fix',
-  '*.html': 'htmlhint',
-  '*.md': 'markdownlint --dot --fix',
-  '*.ts': 'vitest related --run',
+  // '*': [
+  //   'prettier --write --ignore-unknown',
+  //   'cspell lint --no-progress --relative --no-must-find-files --dot --gitignore',
+  // ],
+  // '*.{ts,tsx}': 'bash -c tsc --noEmit',
+  // '*.{js,mjs,cjs,ts,tsx}': 'eslint --fix',
+  // '*.css': 'stylelint --fix',
+  // '*.html': 'htmlhint',
+  // '*.md': 'markdownlint --dot --fix',
+  // '*.ts': 'vitest related --run',
 };

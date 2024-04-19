78f0a82f80e72598fdbf2602eecfd7fc84bdfca0
donniean <acmilank22@gmail.com>
2024-04-20 02:30:35 +0800

    chore: update configs (#770)
    * chore: update configs
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * chore: update dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * chore: upgrade dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * chore: upgrade dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * chore: upgrade dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * build: upgrade dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * build: upgrade dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * build: upgrade dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * build: upgrade dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * build: upgrade dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * build: upgrade dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * chore: update dependencies
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    ---------
    
    Signed-off-by: donniean <acmilank22@gmail.com>

--- configs.config.mjs
+++ configs.config.mjs
@@ -1,4 +1,6 @@
-module.exports = {
+import { defineConfig } from '@donniean/configs';
+
+export default defineConfig({
   features: {
     gitignore: true,
     gitattributes: true,
@@ -35,4 +37,4 @@ module.exports = {
     'lint-staged': true,
     husky: true,
   },
-};
+});

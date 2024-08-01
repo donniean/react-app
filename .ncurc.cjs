92eec9ce9fee82db3691e15947917ec7e239a774
donniean <12584040+donniean@users.noreply.github.com>
2024-08-01 11:36:06 +0800

    chore: update .ncurc.cjs (#958)
    Signed-off-by: donniean <donniean1@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,3 +1,10 @@
 module.exports = {
-  reject: ['eslint', 'eslint-plugin-sonarjs', 'eslint-plugin-vitest'],
+  reject: [
+    '@types/eslint',
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint/parser',
+    'eslint',
+    'eslint-plugin-sonarjs',
+    'eslint-plugin-vitest',
+  ],
 };

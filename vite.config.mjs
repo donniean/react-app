be1b0beb5d411da181922a8acbe598bc50ee8308
donniean <donniean1@gmail.com>
2024-08-08 18:47:59 +0800

    chore: add a comment of eslint-disable-next-line import/no-unresolved in vite.config.mjs
    Signed-off-by: donniean <donniean1@gmail.com>

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,8 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import svgr from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
+// eslint-disable-next-line import/no-unresolved
+import * as tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   css: {

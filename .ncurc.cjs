e663196ff4d40c949ddbb0931b95210626adcdeb
donniean <donniean1@gmail.com>
2024-08-27 18:38:28 +0800

    feat: support ESLint flat config
    Signed-off-by: donniean <donniean1@gmail.com>

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   reject: [
-    '@types/eslint',
-    '@typescript-eslint/eslint-plugin',
-    '@typescript-eslint/parser',
-    'eslint',
-    'eslint-plugin-sonarjs',
-    'eslint-plugin-vitest',
+    // '@types/eslint',
+    // '@typescript-eslint/eslint-plugin',
+    // '@typescript-eslint/parser',
+    // 'eslint',
+    // 'eslint-plugin-sonarjs',
+    // 'eslint-plugin-vitest',
   ],
 };

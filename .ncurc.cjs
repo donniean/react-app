3217e60ddb0fb9b0df422a3c0c67ae2f2ab28718
donniean <12584040+donniean@users.noreply.github.com>
2024-12-26 10:27:08 +0800

    chore(deps): bump dependencies (#1189)
    Signed-off-by: donniean <donniean1@gmail.com>

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     '@typescript-eslint/eslint-plugin',
     '@typescript-eslint/parser',
     'eslint',
+    'eslint-plugin-react-hooks',
     'eslint-plugin-sonarjs',
     'eslint-plugin-vitest',
   ],

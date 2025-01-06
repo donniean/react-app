570d8e62a8f3874b850b5f048fe5e91062021789
donniean <donniean1@gmail.com>
2025-01-06 08:27:29 +0800

    chore: migrate to ESLint flat config
    Signed-off-by: donniean <donniean1@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,37 @@
-export default [];
+// @ts-check
+
+// import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+/* const config = [
+  {
+    ignores: ['.history/', 'coverage/', 'dist/', '.next/'],
+  },
+  eslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.es2025,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'error',
+    },
+  },
+  {
+    files: ['src/'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.devtools,
+      },
+    },
+  },
+]; */
+
+export default tseslint.config(
+  {},
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);

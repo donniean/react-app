fa8b02f442c98ab9aedb3bb845714ee887e9e2c0
donniean <donniean1@gmail.com>
2025-01-07 23:48:05 +0800

    chore: update ESLint configuration and dependencies
    Signed-off-by: donniean <donniean1@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,34 @@
 // @ts-check
 
-// import globals from 'globals';
+import globals from 'globals';
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import typescriptEslint from 'typescript-eslint';
+// import eslintPluginEslintComments from '@eslint-community/eslint-plugin-eslint-comments';
+import eslintPluginPromise from 'eslint-plugin-promise';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import eslintPluginSonarjs from 'eslint-plugin-sonarjs';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import * as eslintPluginImport from 'eslint-plugin-import';
 
-/* const config = [
+export default typescriptEslint.config(
   {
     ignores: ['.history/', 'coverage/', 'dist/', '.next/'],
   },
   eslint.configs.recommended,
+  eslintPluginImport.flatConfigs?.recommended,
+  // eslintPluginEslintComments.configs.recommended,
+  eslintPluginPromise.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: globals.builtin,
+    },
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+    },
+  },
+  eslintPluginSonarjs.configs.recommended,
+  eslintConfigPrettier,
   {
     languageOptions: {
       globals: {
@@ -20,6 +40,24 @@ import tseslint from 'typescript-eslint';
     },
   },
   {
+    files: ['**/*.{mjs,ts,tsx}'],
+    plugins: {
+      'simple-import-sort': eslintPluginSimpleImportSort,
+    },
+    rules: {
+      'sort-imports': 'off',
+      'import/order': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    ...typescriptEslint.configs.recommended,
+    ...typescriptEslint.configs.recommendedTypeChecked,
+    ...typescriptEslint.configs.stylisticTypeChecked,
+  },
+  {
     files: ['src/'],
     languageOptions: {
       globals: {
@@ -28,10 +66,4 @@ import tseslint from 'typescript-eslint';
       },
     },
   },
-]; */
-
-export default tseslint.config(
-  {},
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
 );

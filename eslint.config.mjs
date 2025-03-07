2a61493a68ac3156f60d4e22e718b7a0a276f969
donniean <12584040+donniean@users.noreply.github.com>
2025-03-07 13:28:55 +0800

    chore(eslint): add documentation link for eslint-plugin-query (#1291)
    Signed-off-by: donniean <donniean1@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,6 +32,7 @@ import typescriptEslint from 'typescript-eslint';
 // https://github.com/francoismassart/eslint-plugin-tailwindcss
 // https://github.com/azat-io/eslint-plugin-perfectionist
 // https://github.com/Kenneth-Sills/eslint-config-airbnb-typescript
+// https://tanstack.com/query/latest/docs/eslint/eslint-plugin-query
 
 export default typescriptEslint.config([
   {

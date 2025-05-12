ec268a6f92c4486905a94d41020e473cdaefd630
donniean <donniean1@gmail.com>
2025-05-13 01:22:30 +0800

    chore: reorder ESLint package references for clarity
    Signed-off-by: donniean <donniean1@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,10 +29,12 @@ import typescriptEslint from 'typescript-eslint';
  *
  */
 
-// https://github.com/francoismassart/eslint-plugin-tailwindcss
+// consider the following package
 // https://github.com/azat-io/eslint-plugin-perfectionist
+// https://github.com/NishargShah/eslint-config-airbnb-extended
 // https://github.com/Kenneth-Sills/eslint-config-airbnb-typescript
 // https://tanstack.com/query/latest/docs/eslint/eslint-plugin-query
+// https://github.com/francoismassart/eslint-plugin-tailwindcss
 
 export default typescriptEslint.config([
   {

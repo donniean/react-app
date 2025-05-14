b485d39911eb83764f36c759ef267f4f1c011330
donniean <12584040+donniean@users.noreply.github.com>
2025-05-15 00:19:40 +0800

    chore: remove unused configs and update linting scripts in package.json (#1425)
    * chore: remove unused configs and update linting scripts in package.json
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * chore: clean up README, remove ESLint TODOs, and update ESLint version in package.json
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    * chore: rename lint step in CI workflow for consistency
    
    Signed-off-by: donniean <donniean1@gmail.com>
    
    ---------
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,17 +20,6 @@ import globals from 'globals';
 // eslint-disable-next-line import-x/no-unresolved
 import typescriptEslint from 'typescript-eslint';
 
-/**
- * References
- *
- * https://biomejs.dev/linter/rules-sources/
- * https://github.com/antfu/eslint-config
- * https://github.com/alan2207/bulletproof-react/blob/master/apps/react-vite/.eslintrc.cjs
- * https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
- * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb/index.js
- *
- */
-
 export default typescriptEslint.config([
   {
     name: 'custom/ignores',

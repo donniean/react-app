653c77ea1d214e9aaab74c5cb0a91a9295a291a0
donniean <donniean1@gmail.com>
2025-05-23 10:50:19 +0800

    fix: add comment to eslint rule for clarity
    Signed-off-by: donniean <donniean1@gmail.com>

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -9,7 +9,7 @@ export default {
     'cspell lint --no-progress --no-must-find-files --dot --gitignore',
   ],
   '*.{ts,tsx}': 'bash -c tsc --noEmit',
-  '*.{js,mjs,cjs,ts,tsx}': 'eslint --fix',
+  '*.{js,mjs,cjs,ts,tsx}': 'eslint --fix', // --max-warnings 0
   '*.css': 'stylelint --fix',
   '*.html': 'htmlhint',
   '*.md': 'markdownlint --dot --fix',

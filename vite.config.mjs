bf6f20490f15b6b2c84255dc182b7f2776147c8d
donniean <donniean1@gmail.com>
2025-06-03 17:11:39 +0800

    feat: add VITE_API_BASE_URL to environment and configure proxy in Vite
    Signed-off-by: donniean <donniean1@gmail.com>

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,48 +1,61 @@
 import { lingui } from '@lingui/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { checker } from 'vite-plugin-checker';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
+const cwd = process.cwd();
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, cwd);
+  const apiUrl = env.VITE_API_BASE_URL;
+
+  const proxy = {
+    '/api': apiUrl,
+  };
+
+  return {
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+      },
     },
-  },
-  server: {
-    port: 3000,
-    host: true,
-  },
-  preview: {
-    port: 3000,
-    host: true,
-  },
-  plugins: [
-    tsconfigPaths(),
-    react({
-      babel: {
-        plugins: [
-          'babel-plugin-react-compiler',
-          '@lingui/babel-plugin-lingui-macro',
-        ],
-      },
-    }),
-    lingui(),
-    svgr(),
-    tailwindcss(),
-    checker({
-      overlay: { initialIsOpen: 'error' },
-      typescript: true,
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-        useFlatConfig: true,
-      },
-      stylelint: {
-        lintCommand: 'stylelint ./src/**/*.css',
-      },
-    }),
-  ],
+    server: {
+      port: 3000,
+      host: true,
+      proxy,
+    },
+    preview: {
+      port: 3000,
+      host: true,
+      proxy,
+    },
+    plugins: [
+      tsconfigPaths(),
+      react({
+        babel: {
+          plugins: [
+            'babel-plugin-react-compiler',
+            '@lingui/babel-plugin-lingui-macro',
+          ],
+        },
+      }),
+      lingui(),
+      svgr(),
+      tailwindcss(),
+      checker({
+        overlay: { initialIsOpen: 'error' },
+        typescript: true,
+        eslint: {
+          lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+          useFlatConfig: true,
+        },
+        stylelint: {
+          lintCommand: 'stylelint ./src/**/*.css',
+        },
+      }),
+    ],
+  };
 });

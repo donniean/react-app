859790e0c3d9dbcaa9d0187d629ba3f55e5d10db
donniean <donniean1@gmail.com>
2025-06-08 02:38:19 +0800

    feat: implement custom theme and update styling utilities
    Signed-off-by: donniean <donniean1@gmail.com>

--- src/routes/root/root.tsx
+++ src/routes/root/root.tsx
@@ -1,8 +1,8 @@
 import { Trans } from '@lingui/react/macro';
 import { Box, Title } from '@mantine/core';
-import { clsx } from 'clsx';
 
 import Logo from '@/assets/images/logo.svg?react';
+import { cn } from '@/utils/cn';
 
 import styles from './root.module.css';
 
@@ -10,7 +10,7 @@ export function Root() {
   return (
     <Box className="flex flex-col items-center justify-center p-[32px]">
       <Logo className={styles.logo} height={128} width={128} />
-      <Title className={clsx('text-5xl', styles.title)} order={1}>
+      <Title className={cn('text-5xl', 'text-primary')} order={1}>
         <Trans>Hello React</Trans>
       </Title>
     </Box>

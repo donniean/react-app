8b9906ca341e2870f1391936a571aa0b943e78f4
donniean <donniean1@gmail.com>
2025-08-20 15:04:52 +0800

    style: add text size class to NotFound title for improved visibility
    Signed-off-by: donniean <donniean1@gmail.com>

--- src/routes/errors/not-found.tsx
+++ src/routes/errors/not-found.tsx
@@ -6,7 +6,7 @@ export function NotFound() {
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-y-4">
-      <h1>{t('notFound.title')}</h1>
+      <h1 className="text-5xl">{t('notFound.title')}</h1>
       <Link to="/">{t('notFound.actions.back')}</Link>
     </div>
   );

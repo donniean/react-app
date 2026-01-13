421a13ba8b351844a9602474d2a8c2b14fd721bc
donniean <12584040+donniean@users.noreply.github.com>
2026-01-13 16:44:35 +0800

    chore(prettier): update prettier config type annotation (#1994)
    - extend jsdoc type to include prettier-plugin-tailwindcss options
    - helps editors pick up plugin options for safer config
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('prettier').Config} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
   singleQuote: true,
   plugins: ['prettier-plugin-tailwindcss'],

37ee4b32bcbb5d361b47eb35c1c0a38527a770e0
donniean <12584040+donniean@users.noreply.github.com>
2026-06-23 11:42:13 +0800

    build(deps): update TypeScript to latest (#2432)
    Signed-off-by: donniean <donniean1@gmail.com>

--- .ncurc.mjs
+++ .ncurc.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from 'npm-check-updates';
 
-const majorLockedDependencies = new Set(['@types/node', 'typescript']);
-const blockedDependencyVersions = new Map([
-  // vite-plugin-checker@0.14.3 is missing required runtime files.
-  // https://github.com/fi3ework/vite-plugin-checker/issues/766
-  ['vite-plugin-checker', new Set(['0.14.3'])],
-]);
+const majorLockedDependencies = new Set(['@types/node']);
 
 export default defineConfig({
-  filterResults: (dependencyName, { upgradedVersion }) => {
-    return !blockedDependencyVersions.get(dependencyName)?.has(upgradedVersion);
-  },
   target: (dependencyName) => {
     if (majorLockedDependencies.has(dependencyName)) {
       return 'minor';

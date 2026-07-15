5457668ad0c655f6743319bb8243d98745beea0b
donniean <donniean1@gmail.com>
2026-07-15 16:24:29 +0800

    chore(deps): update project configuration
    - add wrangler to ignored dependencies in knip.config.ts
    - remove unused deploy:preview script from package.json
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- knip.config.ts
+++ knip.config.ts
@@ -2,6 +2,7 @@ import type { KnipConfig } from 'knip';
 
 export default {
   entry: ['.ncurc.mjs', 'html-validate.config.ts', 'i18next.config.ts', 'src/@types/**/*.d.ts'],
+  ignoreDependencies: ['wrangler'],
   ignoreFiles: ['src/components/layouts/base-layout.tsx'],
   ignoreIssues: {
     'src/lib/i18n/index.ts': ['exports'],

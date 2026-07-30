8c06b965f77dcc2608090b5a33ce1df561fd8634
donniean <donniean1@gmail.com>
2026-07-30 11:31:35 +0800

    chore(knip): ignore generated route tree file
    - add src/app/routeTree.gen.ts to ignoreFiles to prevent false positive issues during analysis
    
    Signed-off-by: donniean <donniean1@gmail.com>

--- knip.config.ts
+++ knip.config.ts
@@ -3,7 +3,7 @@ import type { KnipConfig } from 'knip';
 export default {
   entry: ['.ncurc.mjs', 'html-validate.config.ts', 'i18next.config.ts', 'src/@types/**/*.d.ts'],
   ignoreDependencies: ['wrangler'],
-  ignoreFiles: ['src/components/layouts/base-layout.tsx'],
+  ignoreFiles: ['src/app/routeTree.gen.ts', 'src/components/layouts/base-layout.tsx'],
   ignoreIssues: {
     'src/lib/i18n/index.ts': ['exports'],
   },
